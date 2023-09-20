35fa483e7107ea279aaa40e81069501a8b2db678
joseVitor03 <jv681033@gmail.com>
2023-09-20 17:16:52 -0300

    atualização

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,60 +1,49 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import vi from 'vitest';
+import vi, { describe } from 'vitest';
 import App from '../App';
 import { renderWithRouter } from '../utils/renderWithRouter';
 import Login from '../Login/Login';
 // import * as funcs from '../Login/index';
 
-test('Farewell, front-end', () => {
-  it('teste referentes a renderização dos elementos da tela Login', () => {
-    render(<Login />);
+describe('Farewell, front-end', () => {
+  it('teste referentes a renderização dos elementos da tela Login', async () => {
+    renderWithRouter(<App />);
     const inputEmail = screen.getByRole('textbox', {
       name: /email:/i,
     });
-    const inputPassword = screen.getByRole('textbox', {
-      name: /senha:/i,
-    });
-    const button = screen.getByRole('button', {
-      name: /enter:/i,
-    });
+    const inputPassword = screen.getByLabelText(/senha:/i);
+    const button = screen.getByText(/enter/i);
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(button).toBeInTheDocument();
   });
 
   it('testes de verificação de preenchimento de dados e redirecionamento de página.', async () => {
-    render(<Login />);
+    renderWithRouter(<App />);
     const inputEmail = screen.getByRole('textbox', {
       name: /email:/i,
     });
-    const inputPassword = screen.getByRole('textbox', {
-      name: /senha:/i,
-    });
-    const button = screen.getByRole('button', {
-      name: /enter:/i,
-    });
+    const inputPassword = screen.getByLabelText(/senha:/i);
+    const button = screen.getByText(/enter/i);
     expect(button).toBeDisabled();
-    userEvent.type(inputEmail, 'joaozin@gmail.com');
-    userEvent.type(inputPassword, '1234567');
+    await userEvent.type(inputEmail, 'joaozin@gmail.com');
+    await userEvent.type(inputPassword, '1234567');
     expect(button).toBeEnabled();
-    userEvent.click(button);
+    await userEvent.click(button);
+    expect(inputEmail).not.toBeInTheDocument();
   });
   it('testes de verificação de preenchimento de dados de forma incorreta.', async () => {
-    render(<Login />);
+    renderWithRouter(<App />);
     const inputEmail = screen.getByRole('textbox', {
       name: /email:/i,
     });
-    const inputPassword = screen.getByRole('textbox', {
-      name: /senha:/i,
-    });
-    const button = screen.getByRole('button', {
-      name: /enter:/i,
-    });
+    const inputPassword = screen.getByLabelText(/senha:/i);
+    const button = screen.getByText(/enter/i);
     expect(button).toBeDisabled();
     userEvent.type(inputEmail, 'joaozin@gmail');
-    userEvent.type(inputPassword, '1234567');
+    userEvent.type(inputPassword, '123456');
     expect(button).toBeDisabled();
     userEvent.click(button);
   });
