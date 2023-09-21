82678a5ee82cfe5e4f0cf55713a0216c8d450b84
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-09-21 02:30:11 -0300

    Exercise 7 and 8 done, 9 loading...

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouter } from '../utils/renderWithRouter';
+import Header from '../components/Header';
+import Meals from '../components/Meals';
 
 describe('Farewell, front-end', () => {
   it('teste referentes a renderização dos elementos da tela Login', async () => {
@@ -44,4 +46,38 @@ describe('Farewell, front-end', () => {
     expect(button).toBeDisabled();
     userEvent.click(button);
   });
+  // test('Testando o input button click na rota /meals', () => {
+  //   const { user } = renderWithRouter(<App />, { route: '/meals' });
+  //   expect(screen.getByRole('heading', {
+  //     name: /meals/i,
+  //   })).toBeInTheDocument();
+  //   const button = screen.getByRole('button', {
+  //     name: /pesquisar/i,
+  //   });
+  //   expect(button).toBeInTheDocument();
+  //   user.click(button);
+  //   expect(screen.getByTestId('search-top-btn')).toBeInTheDocument();
+  //   user.click(button);
+  // });
+
+  // test('toggle search input in Header when clicking the search button', () => {
+  //   render(<Meals />);
+  //   const searchButton = screen.getByTestId('search-top-btn');
+  //   const searchInput = screen.getByTestId('search-input');
+
+  //   // Verifique se o input de busca não é exibido inicialmente
+  //   expect(searchInput).not.toBeVisible();
+
+  //   // Clique no botão de busca usando userEvent
+  //   userEvent.click(searchButton);
+
+  //   // Verifique se o input de busca agora é visível
+  //   expect(searchInput).toBeVisible();
+
+  //   // Clique novamente no botão de busca usando userEvent
+  //   userEvent.click(searchButton);
+
+  //   // Verifique se o input de busca foi ocultado novamente
+  //   expect(searchInput).not.toBeVisible();
+  // });
 });
