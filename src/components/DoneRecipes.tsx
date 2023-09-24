aa093c7cd6b2a1b478a074ccec8fe8d9fb6b0cd6
joseVitor03 <jv681033@gmail.com>
2023-09-24 11:15:45 -0300

    requisitos 44 ao 49, referente a tela de receitas feitas

--- src/components/DoneRecipes.tsx
+++ src/components/DoneRecipes.tsx
@@ -1,10 +1,133 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import shareIncon from '../images/shareIcon.svg';
 
-export default function DoneRecipes() {
+function DoneRecipes() {
+  type ListType = {
+    id: string,
+    type: string,
+    nationality: string,
+    category: string,
+    alcoholicOrNot: string,
+    name: string,
+    image: string,
+    doneDate: string,
+    tags: string,
+  };
+
+  const [listDone, setListDone] = useState<ListType[]>([]);
+  const [globlalListDone, setGlobalListDone] = useState<ListType[]>([]);
+  const [copy, setCopy] = useState('');
+  const [clicked, setClicked] = useState<number>();
+
+  const handleDrinksFilter = () => {
+    setListDone(globlalListDone.filter((list) => list.type !== 'meal'));
+  };
+
+  const handleMealsFilter = () => {
+    setListDone(globlalListDone.filter((list) => list.type === 'meal'));
+  };
+
+  const handleCopy = (id: string, index: number) => {
+    navigator.clipboard.writeText(`http://localhost:3000/meals/${id}`)
+      .then(() => setCopy('Link copied!'))
+      .catch(() => console.log('erro'));
+    setClicked(index);
+  };
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('doneRecipes') || '[]');
+    setListDone(data);
+    setGlobalListDone(data);
+  }, []);
   return (
     <div>
       <Header title="Done Recipes" showSearchIcon={ false } showProfileIcon />
+      <section>
+        <button
+          onClick={ () => setListDone(globlalListDone) }
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          onClick={ handleMealsFilter }
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          onClick={ handleDrinksFilter }
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </section>
+      <section>
+        {listDone.length > 0 ? (
+          <>
+            {listDone.map((list, index) => (
+              <div key={ list.id }>
+                <Link
+                  to={ list.type === 'meal'
+                    ? `/meals/${list.id}` : `/drinks/${list.id}` }
+                >
+                  <img
+                    style={ { width: '200px', alignItems: 'center' } }
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ list.image }
+                    alt={ list.name }
+                  />
+                </Link>
+                <h6 data-testid={ `${index}-horizontal-top-text` }>
+                  { list.type === 'meal' ? (
+                    <>
+                      {list.nationality}
+                      {' - '}
+                      {list.category}
+                    </>)
+                    : <div>{list.alcoholicOrNot}</div>}
+                </h6>
+                <Link
+                  to={ list.type === 'meal'
+                    ? `/meals/${list.id}` : `/drinks/${list.id}` }
+                >
+                  <h5 data-testid={ `${index}-horizontal-name` }>
+                    {list.name}
+                  </h5>
+                </Link>
+                <p data-testid={ `${index}-horizontal-done-date` }>
+                  {list.doneDate}
+                </p>
+                <button
+                  data-testid={ `btn-Copy${index}` }
+                  style={ { width: '150px' } }
+                  onClick={ () => handleCopy(list.id, index) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    src={ shareIncon }
+                    alt=""
+                  />
+                </button>
+                { clicked === index && <span>{copy}</span> }
+                <div>
+                  <span data-testid={ `${index}-${list.tags[0]}-horizontal-tag` }>
+                    {list.tags[0]}
+                  </span>
+                  {' '}
+                  <span data-testid={ `${index}-${list.tags[1]}-horizontal-tag` }>
+                    {list.tags[1]}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : 'Nenhuma receita feita.'}
+      </section>
     </div>
   );
 }
+
+export default DoneRecipes;
---
+++ src/tests/DoneRecipes.test.tsx
@@ -0,0 +1,125 @@
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import '../utils/localStorage';
+import doneRecipes from '../utils/DoneRecipesMock';
+import { renderWithRouter } from '../utils/renderWithRouter';
+
+describe('testes da página DoneRecipes', () => {
+  const DoneRecipes = 'done-recipes';
+
+  beforeEach(() => {
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+  });
+  afterEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('vereficando se está renderizando os elementos', () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+    const imageSpicy = screen.getByRole('img', {
+      name: /spicy arrabiata penne/i,
+    });
+    const nameSpicy = screen.getByRole('heading', {
+      name: /spicy arrabiata penne/i,
+    });
+    const imageAquamarine = screen.getByRole('img', {
+      name: /aquamarine/i,
+    });
+    expect(imageSpicy).toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+    expect(nameSpicy).toBeInTheDocument();
+  });
+  it('testando rotas', async () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+    const imageSpicy = screen.getByRole('img', {
+      name: /spicy arrabiata penne/i,
+    });
+    expect(imageSpicy).toBeInTheDocument();
+    await userEvent.click(imageSpicy);
+    expect(imageSpicy).not.toBeInTheDocument();
+  });
+
+  it('testando filter Meals', async () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+
+    const imageSpicy = screen.getByRole('img', {
+      name: /spicy arrabiata penne/i,
+    });
+    const imageAquamarine = screen.getByRole('img', {
+      name: /aquamarine/i,
+    });
+    const btnMeals = screen.getByRole('button', {
+      name: /meals/i,
+    });
+    expect(imageSpicy).toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+    await userEvent.click(btnMeals);
+    expect(imageAquamarine).not.toBeInTheDocument();
+    expect(imageSpicy).toBeInTheDocument();
+  });
+
+  it('testando filter Drinks', async () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+
+    const imageSpicy = screen.getByRole('img', {
+      name: /spicy arrabiata penne/i,
+    });
+    const imageAquamarine = screen.getByRole('img', {
+      name: /aquamarine/i,
+    });
+    const btnDrinks = screen.getByRole('button', {
+      name: /drinks/i,
+    });
+
+    expect(imageSpicy).toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+    await userEvent.click(btnDrinks);
+    expect(imageSpicy).not.toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+  });
+
+  it('testando botão de copiar', async () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+    const btnCopy = screen.getByTestId('btn-Copy0');
+    await userEvent.click(btnCopy);
+    expect(screen.getByText(/link copied!/i)).toBeInTheDocument();
+  });
+
+  it('testando filter All', async () => {
+    renderWithRouter(<App />, { route: DoneRecipes });
+    expect(JSON.parse(localStorage.getItem('doneRecipes') || '[]')).toEqual(doneRecipes);
+
+    const imageSpicy = screen.getByRole('img', {
+      name: /spicy arrabiata penne/i,
+    });
+    const imageAquamarine = screen.getByRole('img', {
+      name: /aquamarine/i,
+    });
+    const btnDrinks = screen.getByRole('button', {
+      name: /drinks/i,
+    });
+
+    const btnAll = screen.getByRole('button', {
+      name: /all/i,
+    });
+
+    expect(imageSpicy).toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+    await userEvent.click(btnDrinks);
+    expect(imageSpicy).not.toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+
+    await userEvent.click(btnAll);
+
+    expect(screen.queryByRole('heading', {
+      name: /spicy arrabiata penne/i,
+    })).toBeInTheDocument();
+    expect(imageAquamarine).toBeInTheDocument();
+  });
+});
