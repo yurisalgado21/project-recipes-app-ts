000f81c4a53785220668c5d7f432e245ac55c94f
paulohenrique-amaral <paulopp.amaral@gmail.com>
2023-09-26 08:58:22 -0300

    Concuido requisitos 32 e 33

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -1,8 +1,11 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import useRequestId from '../hooks/useRequestId';
 import Carousel from './Carousel';
+import shareIcon from '../images/shareIcon.svg'
 
 export default function RecipeDetails() {
+  const [isLinkCopied, setLinkCopied] = useState(false);
   const { pathname } = useLocation();
   const isMeal = pathname.includes('meals');
   const recipeId = pathname.split('/')[2];
@@ -20,8 +23,14 @@ export default function RecipeDetails() {
     return (
       <h2>Carregando...</h2>
     );
-  }
+  }  
 
+  const copyToClipboard = () => {
+    const recipeLink = window.location.href; // Obtém o link da receita atual
+    navigator.clipboard.writeText(recipeLink).then(() => {
+      setLinkCopied(true); // Define o estado para indicar que o link foi copiado
+    });
+  };
   return (
     <>
       <img
@@ -29,7 +38,7 @@ export default function RecipeDetails() {
         alt="Foto da receita"
         data-testid="recipe-photo"
       />
-      <h1 data-testid="recipe-title">{value.strMeal || value.strDrink}</h1>
+      <h1 data-testid="recipe-title">{value.strMeal || value.strDrink}</h1>      
       <h3 data-testid="recipe-category">
         {isMeal
           ? value.strCategory
@@ -59,8 +68,24 @@ export default function RecipeDetails() {
           gyroscope;
           picture-in-picture;"
         allowFullScreen
-      />
+      />      
       <Carousel />
+      <div>
+      <button
+          data-testid="share-btn"
+          onClick={copyToClipboard}
+        >
+          <img src={ shareIcon } alt="icon-share" />
+        </button>
+        {isLinkCopied && (
+          <p data-testid="link-copied-message">Link copied!</p>
+        )}
+        <button
+          data-testid="favorite-btn"
+        >
+          favoritar
+        </button>
+      </div>
     </>
   );
 }
