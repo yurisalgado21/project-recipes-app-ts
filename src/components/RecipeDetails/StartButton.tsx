ada4b21894ab5a6006171d8eaaa2e7acab776306
andrew-marquezin <andrewmmarinho@gmail.com>
2023-09-27 17:30:59 -0300

    o botão de continuar receita agora passa nos testes

--- src/components/RecipeDetails/StartButton.tsx
+++ src/components/RecipeDetails/StartButton.tsx
@@ -1,15 +1,44 @@
 export default function StartButton(props: { rcpId: string }) {
   const { rcpId } = props;
-  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes') as string);
-  const doneIds = doneRecipes.map((e: any) => e.id);
-  // console.log(doneRecipes.map((e: any) => e.id));
-  if (!doneIds.includes(rcpId)) {
+  const doneRecipes = localStorage.doneRecipes
+    ? JSON.parse(localStorage.getItem('doneRecipes') as string)
+    : [];
+
+  const doneIds = doneRecipes.length > 0
+    ? doneRecipes.map((e: any) => e.id)
+    : [];
+  // console.log(doneIds);
+  const inProgress = localStorage.inProgressRecipes
+    ? JSON.parse(localStorage.getItem('inProgressRecipes') as string)
+    : { drinks: {}, meals: {} };
+
+  // começar a desenvolver a lógica de como fazer o botão de continuar a receita
+  // os ids das receitas em progresso são as chaves dos objetos no localStorage
+  // um object.keys deve resolver com um includes() e acho que isso é tudo com o que tenho que me preocupar
+  // gostaria de popular o localStorage para testar isso manualmente e ter c
+
+  // console.log(inProgress);
+  // if (Object.keys(inProgress.meals).includes(rcpId)) console.log('🍌');
+
+  if (!doneIds.includes(rcpId)
+        && !Object.keys(inProgress.meals || inProgress.drinks).includes(rcpId)) {
+    console.log('a');
     return (
       <button
         className="start-btn"
         data-testid="start-recipe-btn"
       >
         Start Recipe
+      </button>
+    );
+  } if (Object.keys(inProgress.meals || inProgress.drinks).includes(rcpId)) {
+    console.log('b');
+    return (
+      <button
+        className="start-btn"
+        data-testid="start-recipe-btn"
+      >
+        Continue Recipe
       </button>
     );
   }
--- src/tests/RecipeDetails.test.tsx
+++ src/tests/RecipeDetails.test.tsx
@@ -3,16 +3,18 @@ import { renderWithRouter } from '../utils/renderWithRouter';
 import App from '../App';
 import DataProvider from '../context/DataProvider';
 import doneRecipes from '../utils/DoneRecipesMock';
+import recipesInProgress from '../utils/RecipeInProgressMock';
 
 describe('Testes do componente RecipeDetails', () => {
   beforeEach(() => {
     localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+    localStorage.setItem('inProgressRecipes', JSON.stringify(recipesInProgress));
   });
   afterEach(() => {
     renderWithRouter(<App />);
   });
 
-  it('teste se o componenente é renderizado corretamente', async () => {
+  it('teste se o botão desaparece', async () => {
     renderWithRouter(
       <DataProvider>
         <App />
@@ -48,6 +50,21 @@ describe('Testes do componente RecipeDetails', () => {
     expect(recipeImg).toBeInTheDocument();
     expect(recipeTitle).toBeInTheDocument();
     expect(isAlcoholic).toBeInTheDocument();
+    expect(startBtn).toBeInTheDocument();
+  });
+
+  it('teste se o botão muda', async () => {
+    renderWithRouter(
+      <DataProvider>
+        <App />
+      </DataProvider>,
+      { route: '/drinks/178319' },
+    );
+
+    const recipeImg = await waitFor(() => screen.getByAltText(/Imagem ilustrativa da receita/i));
+    const startBtn = await waitFor(() => screen.getByRole('button', { name: /Continue Recipe/i }));
+
+    expect(recipeImg).toBeInTheDocument();
     expect(startBtn).toBeInTheDocument();
   });
 });
